9b1a925bd622d688101f661f4b19e4a11e836b76
rachanakalchetty <rachanakalchetty@gmail.com>
2019-08-19 04:15:59 +1000

    MasterIAD
    IAD

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.HelloWorld;
+using WebApplication1.Exercise;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This is my first c# program.";
+            HelloWorld.Hello h = new HelloWorld.Hello();
+            ViewBag.Message = h.GetHello();
+            ExampleDictionary ed = new ExampleDictionary();
+   
+            ed.Example();
+
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/HelloWorld/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.HelloWorld
+{
+    public class Hello
+    {
+        public String GetHello()
+        {
+            
+            return "HelloWorld!";
+        }
+    }
+}
